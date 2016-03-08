ab75e9a0c9de76eaf2bf06d1f7bea806cb826fdd
janc93 <j.alejandro.nc@gmail.com>
2016-03-08 07:40:01 -0600

    Update echo_server.c

--- echo_server.c
+++ echo_server.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <time.h>
 
-void handleTCPEchoClient(int clientSocket){
-  char buffer[BUFSIZ];
-  ssize_t bytesRcvd = recv(clientSocket, buffer, BUFSIZ, 0);
-  send(clientSocket, buffer, bytesRcvd, 0);
-  close(clientSocket);
+#define clearScreen() printf("\033[H\033[J")
+
+void handleTCPTimeClient(int clientSocket){
+	time_t rawtime;
+  	struct tm * timeinfo;
+  	time ( &rawtime );
+  	timeinfo = localtime ( &rawtime );
+    char *timeString = asctime(timeinfo);
+    size_t timeStringLength = strlen(timeString);
+    ssize_t numBytes = send(clientSocket,timeString, timeStringLength, 0);   
+	close(clientSocket);
 }
 
-void handleTimeClient(){}
-
 int main(){
-  int serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  struct sockaddr_in serverAddress;
-  memset(&serverAddress, 0, sizeof(serverAddress));
-  serverAddress.sin_family = AF_INET;
-  serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-  serverAddress.sin_port = htons(5000);
-  bind(serverSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress));
-  listen(serverSocket, SOMAXCONN);
-  for(;;){
-    struct sockaddr_in clientAddress;
-    socklen_t clientAddrLen = sizeof(clientAddress);
-    int clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientAddrLen );
-    char clientIp[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &clientAddress.sin_addr.s_addr, clientIp, sizeof(clientIp));
-    printf("%s %d\n", clientIp, ntohs(clientAddress.sin_port));
-    handleTCPEchoClient(clientSocket);
-  }
-  return 0;
+	clearScreen();
+	int serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	struct sockaddr_in serverAddress; 
+	memset(&serverAddress, 0, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY); 
+	serverAddress.sin_port = htons(5000);
+	bind(serverSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress));
+	listen(serverSocket, SOMAXCONN);
+	for(;;){
+		struct sockaddr_in clientAddress;
+		socklen_t clientAddrLen = sizeof(clientAddress);
+		int clientSocket = accept(serverSocket,(struct sockaddr*) &clientAddress, &clientAddrLen);
+		char clientIP[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &clientAddress.sin_addr.s_addr, clientIP, sizeof(clientIP));
+		printf("SOURCE IP   : %s \n", clientIP);
+		printf("SOCKET USED : %d \n",ntohs(clientAddress.sin_port));
+		printf("Date and Time\n\n");
+		handleTCPTimeClient(clientSocket);
+	}
+
+	return 0;
 }
